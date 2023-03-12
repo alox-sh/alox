419a6331311e5b3c17f5a398a90d4a8e15f5d713
Patrik Šimunič <patriksimunic@hotmail.com>
2023-03-12 05:08:03 +0100

    add release script

---
+++ scripts/release.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+if [[ "$1" =~ ^[0-9]+(\.[0-9]+){2}$ ]]; then
+    TAG_NAME="v$1"
+fi
+
+if [[ "$TAG_NAME" == "" ]]; then
+    echo "Invalid version '$1'"
+    echo "Expected version number in format 'x.y.z', where"
+    echo "  x = major version"
+    echo "  y = minor version"
+    echo "  z = fix version"
+    exit 1
+fi
+
+ORIGINAL_BRANCH=$(git symbolic-ref --short HEAD)
+git checkout master
+git pull origin master
+
+go mod tidy
+
+go test ./...
+
+# Reverting a remote git-anything is pain in
+# the ass, better to be sure about the version
+echo ""
+echo "You are about to release a package version $TAG_NAME"
+echo "Are you sure?"
+read SURE
+
+if [[ $SURE != "yes" ]]; then
+    exit 0
+fi
+
+echo "really, Really sure?"
+read SURE
+
+if [[ $SURE != "yes" ]]; then
+    exit 0
+fi
+
+git tag $TAG_NAME
+git push origin $TAG_NAME
+
+git checkout $ORIGINAL_BRANCH
+
+echo "Released package version $TAG_NAME"
